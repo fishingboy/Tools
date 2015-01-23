0d34872f5ce7f8aa13993af6fd0f8050f8e0ace4
Leo.Kuo <et282523@hotmail.com>
2015-01-23 15:46:20 +0800

    bug fix

--- lib/register_globals.php
+++ lib/register_globals.php
@@ -3,7 +3,7 @@
  * 因為 register_globals 已在 php 5.4 版移除，所以先寫個簡單的轉換機制
  */
 $global_values = array($_COOKIE, $_SESSION, $_GET, $_POST);
-foreach ($global_values as $$values)
+foreach ($global_values as $values)
 {
     foreach ($values as $key => $value)
     {
